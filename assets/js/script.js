bf40b67e1eb38aa60bc798068456bfefccc137c6
natizui <izui.nm@gmail.com>
2018-04-11 17:01:58 -0300

    adição de countdown timer bugado, tem que fazer mudar de questão quando zera e zerar quando muda de questão

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,3 +108,58 @@ if( document.getElementById("result") ) {
 
 
 // #endregion
+
+// #region COUNTDOWN TIMER 1:00
+/*
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+if( document.getElementById("quiz") ) {
+    window.onload = function () {
+        var oneMinute = 60 * 1,
+        display = document.querySelector('#countdown');
+        startTimer(oneMinute, display);
+    };
+}
+*/
+// #endregion
+
+
+function startTimer(duration, display) {
+    var timer = duration, seconds;
+    setInterval(
+        function () {
+            seconds = parseInt(timer % 60, 10);
+
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = "00:" + seconds;
+
+            if (--timer < 0) {
+                timer = duration;
+            }
+        }, 
+        1000);
+}
+
+if( document.getElementById("quiz") ) {
+    window.onload = function () {
+        var tenSeconds = 10,
+        display = document.querySelector('#countdown');
+        startTimer(tenSeconds, display);
+    };
+}
